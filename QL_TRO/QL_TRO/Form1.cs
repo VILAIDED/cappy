b0f16881b45113d72b56bc7cac74ea3eada13d25
VILAIDED <65062161+VILAIDED@users.noreply.github.com>
2020-11-06 09:15:10 +0700

    updated...
    new

--- QL_TRO/QL_TRO/Form1.cs
+++ QL_TRO/QL_TRO/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace QL_TRO
 {
     public partial class Form1 : Form
     {
+        static string sql_connect = ConfigurationManager.ConnectionStrings["sql_connect"].ConnectionString;
+        SqlConnection con = new SqlConnection(sql_connect);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            if(con.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Connected");
+            }
+           
         }
     }
 }
