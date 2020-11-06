48feafd79867fc17b0778fa410399f3c5bd45d80
VILAIDED <65062161+VILAIDED@users.noreply.github.com>
2020-11-06 11:05:45 +0700

    Update Service.cs

--- QL_TRO/QL_TRO/dataAccess/Service.cs
+++ QL_TRO/QL_TRO/dataAccess/Service.cs
@@ -8,7 +8,9 @@ namespace QL_TRO.dataAccess
 {
     public class Service
     {
-
-       
+        string findPhongTrong = "select MA_PHONG,TRANG_THAI from (SELECT p.Ma_PHONG MA_PHONG,  Case WHEN HOP_DONG_THUE_PHONG.TRANG_THAI = 'DANG THUE' or HOP_DONG_THUE_PHONG.TRANG_THAI = 'SAP TRA'" +
+                                "THEN HOP_DONG_THUE_PHONG.TRANG_THAI ELSE 'available'  END as TRANG_THAI"
+                                + "FROM PHONG p FULL OUTER JOIN HOP_DONG_THUE_PHONG ON p.MA_PHONG = HOP_DONG_THUE_PHONG.MA_PHONG) as cap where cap.TRANG_THAI = 'DANG THUE'";
+        string 
     }
 }
